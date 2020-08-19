55912e4590aa3ef0e52182865ab4ac79aa4adeaa
Godswill <30760648+WillDera@users.noreply.github.com>
2020-08-19 23:20:41 +0100

    Update
    Development in progress

---
+++ src/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import datetime
+import time
+
+# Introduction Header
+st.title("Speech to Text")
+
+# Introductory text
+st.markdown(
+    "This application allows you to send in an audio file and the convert it to text in nealry any language")
