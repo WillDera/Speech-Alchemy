c1fcdf699f8caab22077cdfa67f7e6616d024fd3
Godswill <30760648+WillDera@users.noreply.github.com>
2020-08-19 23:49:10 +0100

    API
    setting up the api for the project.

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ import os
 from ibm_watson import SpeechToTextV1
 import json
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from decouple import config
 
 # Introduction Header
 st.title("Speech to Text")
@@ -14,6 +15,9 @@ st.title("Speech to Text")
 # Introductory text
 st.markdown(
     "This application allows you to send in an audio file and the convert it to text in nealry any language")
+
+url_s2t = config('URL_S2T')
+api_key = config('API_KEY')
 
 
 def file_selector(folder_path='.'):
@@ -24,3 +28,18 @@ def file_selector(folder_path='.'):
 
 filename = file_selector()
 st.write('You selected `%s`' % filename)
+
+
+# Creating a speech to text Object
+def s2t_object():
+    auth = IAMAuthenticator(api_key)
+    speech2text = SpeechToTextV1(authenticator=auth)
+    speech2text.set_service_url(url_s2t)
+
+    with open(filename, mode='rb') as wav:
+        response = speech2text.recognize(audio=wav, content_type='audio/mp3')
+        return response.result
+
+
+results = s2t_object()
+st.write(results)
