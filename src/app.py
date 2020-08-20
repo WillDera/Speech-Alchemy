07d9aa7c78d5ddd2de65394888ed32042fbe571d
Godswill <30760648+WillDera@users.noreply.github.com>
2020-08-20 20:58:01 +0100

    Feature
    Setting up the feature to allow users select a language they wish to translate to.

--- src/app.py
+++ src/app.py
@@ -5,10 +5,11 @@ import time
 import os
 
 # importing modules for watson
-from ibm_watson import SpeechToTextV1
+from ibm_watson import SpeechToTextV1, LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import json
-from pandas.io.json import json_normalize
+from pandas.io.json import json_normalize  # deprecated
+# from pandas.json_normalize import json_normalize
 
 # Introduction Header
 st.title("Speech to Text")
@@ -19,6 +20,9 @@ st.markdown(
 
 url_s2t = config('URL_S2T')
 api_key = config('API_KEY')
+url_lt = config('URL_LT')
+lt_key = config('LT_KEY')
+lt_version = config('LT_VERSION')
 
 
 def file_selector(folder_path='.'):
@@ -44,6 +48,29 @@ def s2t_object():
 
 
 results = s2t_object()
-st.write(results["results"])
+# st.write(results["results"])
 # [0]["alternatives"][0]["transcript"]
 st.write(json_normalize(results['results'], "alternatives"))
+
+
+# Setting up the language translation feature
+st.subheader("Language Translation")
+
+
+@st.cache(suppress_st_warning=True)
+def languages():
+    # create a language translator object
+    auth = IAMAuthenticator(lt_key)
+    lang_translator = LanguageTranslatorV3(
+        version=lt_version, authenticator=auth)
+    lang_translator.set_service_url(url_lt)
+
+    # Create select box for the options
+    languages = json_normalize(
+        lang_translator.list_identifiable_languages().get_result(), "languages")
+    select_language = st.selectbox("Select language: ", languages)
+    return select_language
+
+
+selected_language = languages()
+st.write(selected_language)
